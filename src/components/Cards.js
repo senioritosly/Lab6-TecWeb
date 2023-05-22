037161a5634d06dfa2dfce8569421996d18b0d1a
Cristales <sirsly073@gmail.com>
2023-05-22 12:40:45 -0600

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -36,7 +36,6 @@ function Cards() {
             setPrev(-1)
             setSelected(0)
             setMoves(moves + 1)
-            setMoves(moves + 1)
             if(items.filter(item => item.stat === "correct").length === items.length){
                 setWin(true)
             }
